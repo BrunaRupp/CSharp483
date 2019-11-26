ed777066110abd0a6cd496859eb043537d481e2c
Bruna Rupp <brunaafrupp@outlook.com>
2019-11-25 22:07:43 -0300

    Aula1 - Tipos

---
+++ CSharp483/CSharp483.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using CSharp483.ConsoleApp.Tipos.TipoValor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp483.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inteiros inteiros = new Inteiros();
+            Flutuante flutuante = new Flutuante();
+            //inteiros.Executar();
+            //flutuante.Executar();
+
+            int numero = 10;
+            int numeroCopia = 0;
+            numeroCopia = numero;
+
+            Console.WriteLine($"Numero1:{numero}");
+            Console.WriteLine($"NumeroCopia:{numeroCopia}");
+            numero = 15;
+            Console.WriteLine($"Numero1:{numero}");
+            Console.WriteLine($"NumeroCopia:{numeroCopia}");
+
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp483/CSharp483.ConsoleApp/Tipos/TipoValor/Flutuante.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp483.ConsoleApp.Tipos.TipoValor
+{
+    class Flutuante
+    {
+        public void Executar()
+        {
+            float numeroComVirgula = 10e6f;
+            double numeroMaiorComVirgula = 1000.85;
+            decimal salarioRafael = 1_000_000.5m;
+
+            Console.WriteLine(numeroComVirgula);
+
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(Single.MaxValue);
+
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(Double.MaxValue);
+
+            Console.WriteLine(decimal.MinValue);
+            Console.WriteLine(Decimal.MaxValue); // Usado para monetário //
+
+            Console.Read();
+        }
+
+    }
+}
